86951399d314c5c7f6cc7603d6cf6446f457608d
Miles Dai <milesdai@mit.edu>
2019-08-24 01:48:38 -0700

    remove build file

--- build/lib/n3s_toolkit/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = [
-    'ciphers',
-    'dictionary',
-    'nutrimatic',
-    'string_proc'
-]
--- build/lib/n3s_toolkit/dictionary.py
+++
@@ -1,9 +0,0 @@
-# Language Dictionaries - not Python dictionaries
-import pkg_resources
-
-def load_english_dict():
-    dictionary = list()
-    dict_path = pkg_resources.resource_filename('n3s_toolkit', 'assets/words_english.txt')
-    with open(dict_path, 'r') as f:
-        dictionary = f.read()
-    return dictionary.split('\n') 
