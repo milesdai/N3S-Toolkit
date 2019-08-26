e2b4ee97a0be30360d27941c2326b23d1202e75c
Miles Dai <milesdai16@gmail.com>
2019-08-26 11:45:48 -0700

    Update __init__.py
    Include encodings module on import.

--- n3s_toolkit/__init__.py
+++ n3s_toolkit/__init__.py
@@ -2,10 +2,12 @@ __all__ = [
     'ciphers',
     'dictionary',
     'nutrimatic',
-    'string_proc'
+    'string_proc',
+    'encodings'
 ]
 
 from . import ciphers
 from . import dictionary
 from . import nutrimatic
 from . import string_proc
+from . import encodings
